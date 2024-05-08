78c4c6923108a9793599ace83e9ff45572907c1c
Diego <diegolima.almeida93@gmail.com>
2024-05-08 17:22:13 -0300

    fix: remove 'e' import in user middlewares

--- src/middlewares/user.middlewares.ts
+++ src/middlewares/user.middlewares.ts
@@ -1,4 +1,4 @@
-import e, { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import prisma from "../server";
 import { AppError } from "../errors";
 
