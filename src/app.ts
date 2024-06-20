3837e8f2d5cdefc2b8d0562b8a257bccb785871d
Ubuntu <ubuntu@instance-20230711-0817>
2024-06-20 13:42:44 -0300

    feat: add humanbenchmark frontend endpoint to white list on app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 
 const app: Application = express();
 
-const whiteList = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const whiteList = ["http://localhost:3000", "http://127.0.0.1:3000", "https://humanbenchmark.sambacore.com"];
 
 const corsOptions = {
   origin(origin: string | undefined, cb: Function) {
